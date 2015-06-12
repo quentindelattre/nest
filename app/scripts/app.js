1eb915f13df5f3139c662b97830c8f3ac0030e17
quentindelattre <hey@quentindelattre.ch>
2015-06-12 14:18:04 +0200

    First commit, added yeoman angular

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nestApp
+ * @description
+ * # nestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
